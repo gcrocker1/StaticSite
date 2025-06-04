66acad8625f84d7d4d14a27a74aae456bc733a98
gcrocker1 <garnetcrocker@gmail.com>
2025-06-04 10:16:54 -0500

    Random add to main, and adjusted config to sync changes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from textnode import TextType, TextNode
+from htmlnode import HTMLNode
 
 
 def main():
     text_node = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
     print(text_node)
+    HTML_node = HTMLNode('a', 'text')
+    print(HTML_node)
 
 if __name__ == "__main__":
     main()
